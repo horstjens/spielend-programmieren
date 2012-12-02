7dc2bf0cd182ff1bd567db09eef7a7398ff8f076
Horst JENS <horstjens@gmail.com>
2012-12-02 14:44:41 +0100

    sprachbaukasten

--- python/sprachbaukasten2.py
+++ python/sprachbaukasten2.py
@@ -1,5 +1,5 @@
 import random
-# verb + präp
+# verb + präposition
 verben=[("beisst",""),("prügelt sich", "mit"),("schaut", "fern mit"),("haut",""),
         ("lernt", "mit"), ("schreibt", "ab von"), ("liebt",""),
         ("bestiehlt",""),("spielt","mit"), ("ärgert",""), ("küsst","")]
@@ -7,11 +7,15 @@ nomen=["Haus","Auto","Flugzeug","Schiff","Bagger","BMW"]
                                        # muss sächlich oder männlihc sein
 mengenangabe=["gar nicht","ein wenig","etwas","ziemlich",
          "sehr", "viel", "zu viel","heftigst","leidentschaftlich","halbherzig"]
+                                       # unbestimmte mengenangabe
 grundwort=["ohne","und denkt dabei an","trotz","wegen","aufgrund von",
            "sich schämend wegen", "aus Rache für"]
-ortsangabe =["in","neben","unter","auf"]#präp und fall 3?
+ortsangabe =["in","neben","unter","auf"]
 zeitangabe = ["Eines Tages", "Danach", "Zuerst", "Morgen", "Nächstes Jahr",
             "Aus heiterem Himmel"]
+adjektiv= ["rostig","rot", "groß","klein","grindig","billig","protzig",
+           "hübsch","schön","elegant","baufällig","im Schlamm versinkend"]
+satzzeichen=[".","!","..."]
 for x in range(5):
     zeit = random.choice(zeitangabe)
     leute=["Horst","Michi","Claudia","Doris","Manfred","Bettina","Joe"]    
@@ -25,5 +29,8 @@ for x in range(5):
     wie = random.choice(mengenangabe)
     grund = random.choice(grundwort)
     wo = random.choice(ortsangabe)
-    print(zeit, verb[0], subj, wie, verb[1], subj2, wo, "einem", obj, grund, subj3)
+    adj = random.choice(adjektiv) + "em"
+    schluss = random.choice(satzzeichen)
+    print(zeit, verb[0], subj, wie, verb[1], subj2, wo, "einem",
+          adj, obj, grund, subj3+schluss)
 
