05f33ea38281b16655aa1bb002968cf196e9fb7f
Horst JENS <horstjens@gmail.com>
2019-08-21 08:56:46 +0200

    Add files via upload

---
+++ python3/2019/week34/dungeon07.py
@@ -0,0 +1,139 @@
+import time
+import random
+
+def door(limit=3):
+    while True:
+        print("press a number between 1 and", limit)
+        answer = input(">>>")
+        try:
+            answer = int(answer)
+        except:
+            print("this was not a number. Try again")
+            continue
+        if answer < 1 or answer > limit:
+            print("between 1 and", limit,"Try again")
+            continue
+        return answer 
+
+def yesno(text="press yes or no and ENTER"):
+    while True:
+        answer = input(text)
+        answer = answer.lower()
+        if answer == "yes" or answer == "y":
+            return True
+        if answer == "no" or answer == "n":
+            return False
+        print("wrong answer. Try again")
+        
+class Monster():
+    
+    number = 0
+    zoo = {}
+    
+    def __init__(self):
+        self.hitpoints = random.randint(10, 30)
+        self.attack = random.randint(1,6)
+        self.defense = random.randint(1,6)
+        self.damage = random.randint(1,6)
+        self.weapon = random.choice(("claw", "bite", "punch", "kick", "horn"))
+        self.armor = random.choice(("fur", "scales", "mane", "shell", "hide"))
+        self.legs = random.choice((2,2,2,4,4,4,4,4,8,8,0,0,100))
+        self.arms = random.choice((2,2,2,2,2,4,4,8,3,0,0,0,0,0))
+        self.heads = random.choice((1,1,1,1,1,1,2))
+        self.adj = random.choice(("horrible", "weird", "strange", "ugly", 
+                                  "very ugly", "smelly", "slimy", "wild"))
+        self.size = random.choice(("tiny", "small", "medium", "medium", "large",
+                                   "gigantic", "titanic"))
+        self.movement = random.choice(("creeping", "crawling", "running", 
+                                       "sneaking", "hobbling", "jumping",
+                                       "flapping", "walking"))
+        self.text = "{} {} {} creature with {} legs, {} arms and {} heads".format(
+                     self.size, self.adj, self.movement, self.legs, self.arms,
+                     self.heads)
+        
+    def describe(self):
+        print(self.text)             
+        
+        
+        
+        
+                    
+        
+
+# ------- player stats ------- 
+hitpoints = 100
+gold = 0
+crazy = 0
+princess = 0
+fame = 0
+
+while hitpoints > 0 and crazy < 100:
+    print("Hitpoints: {} Crazyness: {}% Gold: {} Princesses: {} Fame: {}".format(
+          hitpoints, crazy, gold, princess, fame))
+    rooms = []
+    for i in range(random.randint(2,6)):
+        rooms.append(random.choice(("monster","monster", "nothing","princess",
+                                    "shop", "stair")))
+    print("You see several doors. Please choose the number of the door:")
+    for nr, what in enumerate(rooms):
+        print(nr+1, "......", "room with", what)
+    answer = door(len(rooms))
+    print("You open the door. You see:", rooms[answer-1])
+    # ----- event -----
+    effect = rooms[answer-1]
+    if effect == "princess":
+        princess +=1
+        fame += 10
+        print("You rescue (another) princess. Hurraaa!")
+    if effect == "monster":
+        Monster().describe()
+        damage = random.randint(1, 15)
+        print("You fight the monster, but you loose {} hp!".format(damage))
+        # hitpoints = hitpoints - damage
+        hitpoints -= damage 
+        fame += 1
+        # ----- loot ? ------
+        if random.random() < 0.35:     # 0.35 = 35% chance
+            loot = random.randint(1, 10)
+            print("You find {} gold in the dead monster.".format(loot))
+            gold += loot
+
+    if effect == "stair":
+        print("You can end the game now. Do you want to exit the dungeon?")
+        if yesno():
+            break
+    if effect == "shop":
+        if gold < 10:
+            print("You find a healer, but you have not enough gold")
+        else:
+            print("Do you want to spend 10 gold for healing? Works almost always!")
+            if yesno():
+                gold -= 10
+                healing = ["good", "good", "good", "good", "good", "good",
+                           "good", "good", "good", "very good", "not at all",
+                           "not at all", "not at all", "bad", "bad",
+                           "very bad"]
+                outcome = random.choice(healing)
+                print("The healing process works {}".format(outcome))
+                if outcome == "not at all":
+                    heal = 0
+                elif outcome == "bad":
+                    heal = random.randint(-6, -1)
+                elif outcome == "very bad":
+                    heal = random.randint(-18, -12)
+                elif outcome == "good":
+                    heal = random.randint(1,6)
+                elif outcome == "very good":
+                    heal = random.randint(7, 18)
+                print("your hitpoints change by {}".format(heal))
+                if hitpoints > 100:
+                    print("you already reached your maximum healt of 100")
+                    hitpoints = 100
+                hitpoints += heal
+                input("press ENTER to continue")
+
+print("Game Over")                
+                
+                
+        
+
